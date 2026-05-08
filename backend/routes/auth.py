2683bb28036a3ddd94858a81800d6511be8f59d9
Madhuri-0607 <mothukurimadhuri96@gmail.com>
2026-05-08 17:09:42 +0530

    Temporary admin signup

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -52,7 +52,7 @@ def signup():
 
     # Create new user (always as member)
     hashed_pw = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
-    user = User(name=name, email=email, password=hashed_pw, role="member")
+    user = User(name=name, email=email, password=hashed_pw, role="admin")
     
     try:
         db.session.add(user)
